bfbad20bfceaa0163038ffd3d34bf00ccb169729
Kyle C <kylecilia@gmail.com>
2019-11-16 12:07:15 -0800

    Populates list of breweries HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,40 @@
 var getBreweries = (searchParam) => {
+  var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + searchParam;
+  console.log('queryURL', queryURL);
   $.ajax({
-    url: "https://api.openbrewerydb.org/breweries?" + "by_city=" + searchParam,
+    url: queryURL,
     method: "GET",
     success: (data) => {
       var list = data;
-      console.log("Brewery list:", JSON.stringify(list));
+      // console.log("Brewery list:", JSON.stringify(list));
+      list.forEach(element => {
+        if(element.brewery_type!=="planning") {
+          var address = `${element.street}, ${element.city}, ${element.state}`;
+          var name = element.name;
+          var type = element.brewery_type;
+          var website = element.website_url;
+          console.log('website', website)
 
-      console.log()
+          var breweryDiv = $("<div>");
+          var breweryName = $("<h2>");
+          var breweryType = $("<h5>");
+          var breweryAddress = $("<p>");
 
+          var breweryWebsite = $("<a>");
+          
+          breweryName.text(name);
+          breweryType.text(type);
+          breweryAddress.text(address);
+          breweryWebsite.text(website);
+          breweryWebsite.attr('href', website);
+          breweryDiv.append(breweryName, breweryType, breweryAddress, breweryWebsite);
+          $('.brew-info').append(breweryDiv);
+          console.log(element);
+          // console.log(`Brewery name: ${element.name}`);
+          // console.log(`Brewery type: ${element.brewery_type}`);
+          // console.log(address);
+        }
+      });
     }
   });
 }
