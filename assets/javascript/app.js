f36c1c282fe41a366f7492a51410d5e0f84af5a2
Anjali Sharma <anjalishar89@gmail.com>
2019-11-21 20:06:03 -0800

    adds updated html and css files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,43 @@
+/*
+  GLOBAL VARIABLES
+*/
 
-var getBreweries = (searchParam) => {
+// mapbox public api key
+var publicToken =
+  "pk.eyJ1IjoiZnJlc2hndWF2YXMiLCJhIjoiY2szM3k3Y2tmMHJmYTNjczJiNDVnZzhvOCJ9.Ry3fBcfenPpbHq86OrbN0Q";
+mapboxgl.accessToken = publicToken;
+// console.log('mapboxgl.accessToken', mapboxgl.accessToken);
+
+// mapboxSDK variable
+var mapboxClient = mapboxSdk({ accessToken: publicToken });
+
+// variable to store map object, draws map
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 10
+});
+
+var currentMarkers = [];
+
+var getBreweries = searchParam => {
   // URL for ajax query
-  var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + searchParam;
-  console.log('queryURL', queryURL);
+  var queryURL =
+    "https://api.openbrewerydb.org/breweries?by_city=" + searchParam;
+  console.log("queryURL", queryURL);
   // empties brew-info div (old search results)
-  $('.brew-info').empty();
+  $(".brew-info").empty();
   // ajax query
   $.ajax({
     url: queryURL,
     method: "GET",
-    success: (data) => {
+    success: data => {
       var list = data;
-      // console.log("Brewery list:", JSON.stringify(list));
+
       // iterate through each element from ajax query
       list.forEach(element => {
         // planning type breweries are not yet launched!
-        if(element.brewery_type!=="planning") {
+        if (element.brewery_type !== "planning") {
           // grabbing useful data about each brewery
           var address = `${element.street}, ${element.city}, ${element.state}`;
           var name = element.name;
@@ -40,9 +62,9 @@ var getBreweries = (searchParam) => {
           breweryAddress.text(address);
           breweryWebsite.text(website);
           breweryPh.text(`${phoneNo}`);
-          breweryWebsite.attr('href', website);
-          
+
           // append info to a container div
+
           //breweryDivTitle.append(breweryName);
           brewCard.append(breweryName, mapIt, breweryType, breweryAddress, breweryPh, breweryWebsite,);
           //breweryDiv.append(breweryDivTitle, breweryDivBody);
@@ -72,69 +94,8 @@ $('.search-btn').click( () => {
   getBreweries(searchInput);
 });
 
-// logs user location - TO BE USED LATER
-var logLocation = (position) => {
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-  console.log(`LAT: ${lat} | LONG: ${lon}`);
-}
-
-// gets user coordinates, callback function is logLocation for now
+// function for user location
 var getUserLocation = () => {
-  if('geolocation' in navigator) {
-    console.log('geolocation available');
-    navigator.geolocation.getCurrentPosition(logLocation);
-  } else {
-    console.log('geolocation not available');
-  }
-}
-
-var addUserLocation = () => {
-  var marker = new mapboxgl.Marker().setLngLat([-122, 37]).addTo(map);
-}
-
-// function for drawing map [WIP]
-var mapThings = (token) => {
-  mapboxgl.accessToken = token;
-  console.log('mapboxgl.accessToken', mapboxgl.accessToken);
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    zoom: 10,
-  });
-
-  // mapbox SDK, necessary for adding markers on the map
-  var mapboxClient = mapboxSdk({accessToken: token})
-
-  // geocoder feature for map, not sure if necessary [WIP]
-  var geocoder = new MapboxGeocoder({
-    accessToken: token,
-    mapboxgl: mapboxgl,
-    marker: false,
-    placeholder: 'Search for a brewery!',
-  });
-
-  mapboxClient.geocoding.forwardGeocode({
-    // query for location, going to have to pass a search in to place marker
-    query: '390 Capistrano Rd, Half Moon Bay, CA',
-    autocomplete: false,
-    // limit to 1 feature so there isn't a map full of markers :P
-    limit: 1,
-  }).send().then((response) => {
-    console.log('response:', response);
-    // if response exists and has a feature on the map
-    if(response && response.body && response.body.features && response.body.features.length) {
-      // feature is first returned element in list - address
-      var feature = response.body.features[0];
-      // center map on feature [WIP]
-      map.center = feature.center;
-      // map zoom on feature [WIP]
-      // map.zoom = 0;
-      // set marker on feature and add it to the map
-      new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
-    }
-  });
-
   // geolocator button
   var geolocate = new mapboxgl.GeolocateControl({
     positionOptions: {
@@ -143,10 +104,93 @@ var mapThings = (token) => {
     trackUserLocation: true
   });
 
-  // adds geocoder to the map client
-  map.addControl(geocoder);
+  var addUserLocationMarker = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const userCoords = [position.coords.longitude, position.coords.latitude];
+      new mapboxgl.Marker({
+        color: "#0fff1b"
+      })
+        .setLngLat(userCoords)
+        .addTo(map);
+      map.flyTo({
+        center: userCoords,
+        zoom: 14
+      });
+    });
+  };
   // adds geolocator button to map client
   map.addControl(geolocate);
-}
+  // adds a marker on user location after loading the webpage
+  addUserLocationMarker();
+};
 
-mapThings(mapBoxKey);
+// SAFETY FIRST
+$(document).ready(function() {
+  getUserLocation();
+
+  // event handler for search
+  $(".addButton").click(function() {
+    // gets search box input
+    var searchInput = $(".searchBox")
+      .val()
+      .trim();
+    console.log("searchInput", searchInput);
+    // gets brewery based on searchInput
+    getBreweries(searchInput);
+  });
+
+  $(document).on("click", ".brewery", function() {
+    var addressOnClick = $(this).attr("data-address");
+    // debug
+    console.log("addressOnClick DEBUG::", addressOnClick);
+    // debug
+    var clicked = $(this);
+    console.log("DEBUG::", clicked);
+
+    // takes in an address from a click event
+    var showFeature = clickSearchQuery => {
+      if (currentMarkers) {
+        currentMarkers.forEach(element => {
+          element.remove();
+        });
+      }
+
+      mapboxClient.geocoding
+        .forwardGeocode({
+          // query for location, going to have to pass a search in to place marker
+          // query: '390 Capistrano Rd, Half Moon Bay, CA',
+          query: clickSearchQuery,
+
+          autocomplete: false,
+          // limit to 1 feature so there isn't a map full of markers :P
+          limit: 1
+        })
+        .send()
+        .then(response => {
+          console.log("response:", response);
+          // if response exists and has a feature on the map
+          if (
+            response &&
+            response.body &&
+            response.body.features &&
+            response.body.features.length
+          ) {
+            // feature is first returned element in list - address
+            var feature = response.body.features[0];
+
+            // zoom on marker
+            map.zoom = 20;
+            // set marker on feature and add it to the map
+            map.flyTo({
+              center: feature.geometry.coordinates
+            });
+            var marker = new mapboxgl.Marker()
+              .setLngLat(feature.center)
+              .addTo(map);
+            currentMarkers.push(marker);
+          }
+        });
+    };
+    showFeature(addressOnClick);
+  });
+});
